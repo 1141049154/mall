385f7292c233ca96daf5d3d69cae3df076e7cdf1
lazy <1141049154@qq.com>
2021-08-11 13:44:31 +0800

    xqfirst

--- src/network/detail.js
+++ src/network/detail.js
@@ -8,3 +8,28 @@ export function getdetaildata(iid){
        }
    })
 }
+
+//商品信息
+export class Goods{
+    constructor(itemInfo,columns,services){
+      this.title = itemInfo.title;
+      this.desc = itemInfo.desc;
+      this.newPrice = itemInfo.price;
+      this.oldPrice = itemInfo.oldPrice;
+      this.discount = itemInfo.discountDesc;
+      this.columns = columns;
+      this.services = services;
+      this.realPrice = itemInfo.lowNowPrice;
+    }
+} 
+
+//商家信息
+export class Shop {
+	constructor(shopInfo) {
+	    this.shopLogo = shopInfo.shopLogo;
+		this.name = shopInfo.name;
+		this.score = shopInfo.score;
+		this.cSells = shopInfo.cSells;
+		this.cGoods = shopInfo.cGoods;
+	}
+}
--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,7 +1,11 @@
 <template>
-  <div >
-   <detail-nav-bar ></detail-nav-bar>
-   <detail-swiper :topimg=topimage></detail-swiper>
+  <div id="detail">
+   <detail-nav-bar class="detail-navbar"></detail-nav-bar>
+   <scroll class="detail-content">
+     <detail-swiper :topimg="topimage"></detail-swiper>
+     <detail-base-info :goods="goods"></detail-base-info>
+     <detail-shop-info :shop="shop"></detail-shop-info>
+   </scroll>
   </div>
 </template>
 
@@ -9,24 +13,34 @@
 import DetailNavBar from "./childComps/DetailNavBar.vue"
 import {getdetaildata} from "../../network/detail.js";
 import DetailSwiper from './childComps/DetailSwiper.vue';
+import {Goods,Shop} from "../../network/detail.js";
+import DetailBaseInfo from "./childComps/DetailBaseInfo.vue";
+import DetailShopInfo from "./childComps/DetailShopInfo.vue";
+import Scroll from "../../components/common/scroll/Scroll.vue"
 
 export default {
   name: 'Detail',
   components: {
   DetailNavBar,
-  DetailSwiper
+  DetailSwiper,
+  DetailBaseInfo,
+  DetailShopInfo,
+  Scroll 
   },
   data(){
     return{
       iid:null,
-      topimage:[]
+      topimage:[],
+      goods:{},
+      shop:{}
     }
   },
   created(){
     this.iid = this.$route.params.iid;
     getdetaildata(this.iid).then( res => {
       this.topimage = res.result.itemInfo.topImages
-      
+      this.goods = new Goods(res.result.itemInfo,res.result.columns,res.result.shopInfo)
+      this.shop = new Shop(res.result.shopInfo)
     })
       
   },
@@ -37,4 +51,19 @@ export default {
 }
 </script>
 <style>
+#detail{
+  position: relative;
+  z-index: 9;
+  background-color:#fff;
+  height:100vh;
+}
+.detail-content{
+  height:calc(100% - 44px);
+ 
+}
+.detail-navbar{
+  position: relative;
+  z-index: 9;
+  background-color: #fff;
+}
 </style>
